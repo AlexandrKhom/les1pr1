89adf9982b85fc6b3d5c56113735a89fed8e1aa1
AlexandrKhom <alex988andr@gmail.com>
2021-01-28 18:43:09 +0200

    les12801

---
+++ src/data/users.ts
@@ -0,0 +1,7 @@
+export const users= [
+  {name: 'alex', age: 22, status: true},
+  {name: 'zay', age: 11, status: false},
+  {name: 'kot', age: 33, status: true},
+  {name: 'ira', age: 44, status: false},
+  {name: 'zina', age: 55, status: true},
+];
